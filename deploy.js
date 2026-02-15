1b6696057b09d49184f7c02f12f8aa9bc93c23af
raymen-lagerbot <rayonixo91@gmail.com>
2026-02-15 18:17:09 +0100

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,58 +1,31 @@
-require('dotenv').config();
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+require("dotenv").config();
+const {REST, Routes, SlashCommandBuilder} = require("discord.js");
+
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 
 const commands = [
+  new SlashCommandBuilder().setName("lager").setDescription("Bucht in Sheet")
+    .addStringOption(o=>o.setName("aktion").setDescription("rein/raus").setRequired(true))
+    .addStringOption(o=>o.setName("item").setDescription("Item").setRequired(true))
+    .addIntegerOption(o=>o.setName("menge").setDescription("Menge").setRequired(true)),
 
-  new SlashCommandBuilder()
-    .setName('lager')
-    .setDescription('Lager Eintrag erstellen')
-    .addStringOption(o =>
-      o.setName('user')
-        .setDescription('Name')
-        .setRequired(true))
-    .addStringOption(o =>
-      o.setName('item')
-        .setDescription('Ware')
-        .setRequired(true))
-    .addIntegerOption(o =>
-      o.setName('menge')
-        .setDescription('Menge')
-        .setRequired(true)),
+  new SlashCommandBuilder().setName("auswertung").setDescription("Wochen Report")
+    .addStringOption(o=>o.setName("woche").setDescription("Editor z.B. 2026-07")),
 
-  new SlashCommandBuilder()
-    .setName('route')
-    .setDescription('Route eintragen')
-    .addStringOption(o =>
-      o.setName('fahrer')
-        .setDescription('Fahrer')
-        .setRequired(true))
-    .addIntegerOption(o =>
-      o.setName('kisten')
-        .setDescription('Kisten')
-        .setRequired(true)),
+  new SlashCommandBuilder().setName("ausbezahlt").setDescription("Markiert Paid")
+    .addStringOption(o=>o.setName("woche").setDescription("z.B. 2026-07").setRequired(true))
+    .addStringOption(o=>o.setName("user").setDescription("z.B. Raymen").setRequired(true)),
+].map(c=>c.toJSON());
 
-  new SlashCommandBuilder()
-    .setName('report')
-    .setDescription('Wochenreport abrufen')
+const rest = new REST({version:"10"}).setToken(DISCORD_TOKEN);
 
-].map(c => c.toJSON());
-
-const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
-
-(async () => {
+(async()=>{
   try {
-    console.log('Deploying commands...');
-
-    await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID
-      ),
-      { body: commands }
-    );
-
-    console.log('Commands deployed.');
-  } catch (err) {
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID,GUILD_ID), {body:commands});
+    console.log("⚡ Commands deployed");
+  } catch(err) {
     console.error(err);
   }
 })();
